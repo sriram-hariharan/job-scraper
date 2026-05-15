f1d85194b88cea40fc3fb599c6cf835374f2caa4
sriram-hariharan <hariram01997@gmail.com>
2026-05-14 22:45:25 -0400

    Stage seen jobs until user pipeline succeeds

--- src/storage/user_pipeline/read_postgres.py
+++ src/storage/user_pipeline/read_postgres.py
@@ -6,6 +6,9 @@ from src.storage.user_pipeline.store import (
     get_user_pipeline_runs_postgres_payload,
     get_user_seen_jobs_postgres_payload,
     is_user_seen_job_postgres_payload,
+    upsert_user_seen_job_staging_postgres_payload,
+    promote_user_seen_jobs_staging_postgres_payload,
+    clear_user_seen_jobs_staging_postgres_payload,
 )
 
 __all__ = [
@@ -14,4 +17,7 @@ __all__ = [
     "get_user_pipeline_runs_postgres_payload",
     "get_user_seen_jobs_postgres_payload",
     "is_user_seen_job_postgres_payload",
+    "upsert_user_seen_job_staging_postgres_payload",
+    "promote_user_seen_jobs_staging_postgres_payload",
+    "clear_user_seen_jobs_staging_postgres_payload",
 ]

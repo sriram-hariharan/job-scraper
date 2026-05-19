4e8c7f664b914b5b3835ae3a8d1a088924cb2699
sriram-hariharan <hariram01997@gmail.com>
2026-05-18 23:24:46 -0400

    Refresh role expansion regression expectations

--- src/app/profile_ui.py
+++ src/app/profile_ui.py
@@ -406,7 +406,7 @@ def saved_scans_page() -> str:
 
   <script src="/static/vendor/tabler/tabler.min.js"></script>
   <script src="/static/shell.js?v=role_onboarding_r6"></script>
-  <script src="/static/profile.js?v=profile_resume_roles_r8"></script>
+  <script src="/static/profile.js?v=profile_saved_scans_e5_discard_icon_profile_resume_roles_r8"></script>
 </body>
 </html>
     """.strip()
